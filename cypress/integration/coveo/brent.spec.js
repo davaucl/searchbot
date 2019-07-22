e42d690a0a297e7b8a5179bd6cbc85dec9dcfcb0
David Auclair <davaucl@gmail.com>
2019-07-22 13:31:04 -0400

    adding personas

--- cypress/integration/coveo/brent.spec.js
+++ cypress/integration/coveo/brent.spec.js
@@ -3,7 +3,8 @@
 it('runs a query and a click on normal behavior site ', () => {
   cy.visit('https://search.cloud.coveo.com/pages/searchasyoutype0j90vzc7/searchasyoutype?csrf_token=b3cd0f70-bfb3-40e4-a366-d6942192dd8f#sort=relevancy')
   cy.get('#loginWithGoogle').click()
-  cy.searchasyoutype('beans', 'Heinz Five Beans 415g')
-  cy.searchasyoutype('milk', 'Cravendale Purefilter Whole Milk 2L')
-  cy.searchasyoutype('orange', 'Innocent Smooth Orange Juice 900ml')
+  cy.searchasyoutype('carrot', "Sainsbury's Iceberg Lettuce 200g")
+  cy.searchasyoutype('lettuce', "Sainsbury's Iceberg Lettuce 200g")
+  cy.searchasyoutype('bleach', "Sainsbury's Thin Bleach, Basics 2L")
 })
+
---
+++ cypress/integration/coveo/rupert.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />
+
+it('runs a query and a click on normal behavior site ', () => {
+  cy.visit('https://search.cloud.coveo.com/pages/searchasyoutype0j90vzc7/searchasyoutype?csrf_token=b3cd0f70-bfb3-40e4-a366-d6942192dd8f#sort=relevancy')
+  cy.get('#loginWithGoogle').click()
+  cy.searchasyoutype('carrot', "Sainsbury's Iceberg Lettuce 200g")
+  cy.searchasyoutype('lettuce', "Sainsbury's Iceberg Lettuce 200g")
+  cy.searchasyoutype('bleach', "Sainsbury's Thin Bleach, Basics 2L")
+})
